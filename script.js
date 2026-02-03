53359189f9f572663f498dac323a1f2987275fa5
Francis Remoshan <remoshanfrancis123@gmail.com>
2026-02-04 02:29:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.getElementById('login-form').classList.remove('hidden');
+    document.getElementById('register-form').classList.add('hidden');
+}
+
+function showRegister() {
+    document.getElementById('register-form').classList.remove('hidden');
+    document.getElementById('login-form').classList.add('hidden');
+}
